d426c42001e4c93076165362741073e6079e8158
Steven <Zsteven44@gmail.com>
2016-04-30 19:09:38 -0400

    Handlebars for templating added to basic views

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var app = express();
+// handlebars setup
+var handlebars = require('express3-handlebars').create({defaultLayout:'main'});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
 
 app.set('port', process.env.PORT || 3000);
 
