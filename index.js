2be2ff143fad968d37e4cb6ca057d7f25aba2f12
Steven <Zsteven44@gmail.com>
2016-05-01 00:36:13 -0400

    Bootstrap enabled

--- index.js
+++ index.js
@@ -2,38 +2,37 @@ var express = require('express');
 var app = express();
 // handlebars setup
 var handlebars = require('express3-handlebars').create({defaultLayout:'main'});
-
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
-
 app.set('port', process.env.PORT || 3000);
 
+
+
+
+app.use(express.static(__dirname + 'public'));
+
 app.get('/', function(req,res){
-    res.type('text/plain');
-    res.send('ggscrub')
+    res.render('home');
 });
 
 app.get('/about', function(req,res){
-    res.type('text/plain');
-    res.send('About ggscrub');
+    res.render('about');
 });
 
-g
 
 
 
 // custom 404 page
 app.use(function(req,res){
-    res.type('text/plain');
     res.status(404);
-    res.send('404 - Not Found');
+    res.render('404');
 });
 //custom 500 page
 app.use(function(err, req, res, next){
-    res.type('text/plain');
+    console.error(err.stack);
     res.status(500);
-    res.send('500 - Server Error');
+    res.render('500');
 });
 
 app.listen(app.get('port'), function(){
