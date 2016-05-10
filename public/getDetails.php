a9ae21dff217e22a173db9b78543b6efc7539e45
Steven <Zsteven44@gmail.com>
2016-05-10 12:14:02 -0400

    Added PHP and Ajax for window updates

---
+++ public/getDetails.php
@@ -0,0 +1,12 @@
+<?php
+$details = array (
+    'item5' =>  "<p> yo yo yo item 5 here </p>",
+	'item1'	=>	"<p>item 1.</p>",
+	'item2'	=>	"<p>Item 2.</p>",
+	'item3'	=>	"<p>Item 3.</p>",
+	'item4'	=>	"<p>Item 4.</p>"
+);
+
+echo $details[$_REQUEST['ImageID']];
+
+?>
--- public/javascript/ajaxsetup.js
+++ public/javascript/ajaxsetup.js
@@ -14,20 +14,48 @@ for (var i=0; i < thumbs.length; i++) {
         detailURL = 'img/' + this.title + '-detail.jpg';
         document.getElementById("itemDetail").src = detailURL;
         getDetails(this.title);
+        console.log(this);
         }
     }
 }
 
-function displayDetails() {
-
-}
-
-function getDetails() {
-
-
-}
 
 function createRequest() {
+  try {
+    request = new XMLHttpRequest();
+  } catch (tryMS) {
+    try {
+      request = new ActiveXObject("Msxml2.XMLHTTP");
+    } catch (otherMS) {
+      try {
+        request = new ActiveXObject("Microsoft.XMLHTTP");
+      } catch (failed) {
+        request = null;
+      }
+    }
+  }
+  return request;
+}
 
+function getDetails(itemName) {
+  request = createRequest();
+  if (request == null) {
+    alert("Unable to create request");
+    return;
+  }
+  var url= "getDetails.php?ImageID=" + escape(itemName);
+  console.log(itemName);
+  console.log(request);
+  request.open("GET", url, true);
+  request.onreadystatechange = displayDetails;
+  request.send(null);
+}
 
+function displayDetails() {
+  if (request.readyState == 4) {
+    if (request.status == 200) {
+      detailDiv = document.getElementById("description");
+      detailDiv.innerHTML = request.responseText;
+    }
+  }
 }
