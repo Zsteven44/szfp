650a1ff39c680310795400d86932916a3f150e26
Steven <Zsteven44@gmail.com>
2016-05-15 16:31:25 -0400

    Working on registration

--- public/javascript/validation.js
+++ public/javascript/validation.js
@@ -3,6 +3,7 @@ window.onload = initPage1;
 function initPage1() {
     document.getElementById('username').onblur =
         checkUsername;
+    document.getElementById('register').disabled = true;
 }
 
 function checkUsername() {
@@ -20,7 +21,7 @@ function checkUsername() {
         var url = ('checkName.php?username=' + username);
         console.log(url);
         request.onreadystatechange = showUsernameStatus;
-        request.open("Get", url, true);
+        request.open("GET", url, true);
         request.send(null);
 
         }
@@ -29,16 +30,24 @@ function checkUsername() {
 function showUsernameStatus () {
     console.log('showUsernameStatus running, state: ' + request.readyState + ' and ready status: ' + request.status);
     //Update the page to show whether the username is okay.
+
     if (request.readyState == 4) {
         if (request.status == 200) {
+            console.log(username.value);
             if (request.responseText == 'okay') {
             document.getElementById('username').className = 'approved';
+            document.getElementById('register').disabled = false;
             console.log(request.responseText);
             console.log('okay');
             // if its okay, no error message ot show
             } else {
             //if theres a problem, we'll tell the user
             document.getElementById('username').className = 'denied';
+            document.getElementById('register').disabled = true;
+            //document.getElementById('username').focus();
+            //document.getElementById('username').select();
+
+
             console.log(request.responseText);
             console.log('denied');
             }
