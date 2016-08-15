f7840dc5418116c2e0108f11d2eb9dfb3c483de5
Steven <Zsteven44@gmail.com>
2016-08-15 13:36:02 -0400

    DB pass and port number changed for public use

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ var handlebars = require('express3-handlebars').create({defaultLayout:'main'});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 80);
 
 // session setup
 app.use(logger("short")); // This is newly added,
